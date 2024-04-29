5b5e69a440aca13b74042029e40709e24866fd94
Ryan McGee <ramtech51@comcast.net>
2024-04-29 12:17:05 -0500

    Added basic CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,94 @@
 mod ledmatrix;
 mod matrix;
 mod widget;
-use std::{process::exit, thread, time::Duration};
+use std::{
+    env::{args, args_os},
+    process::exit,
+    thread,
+    time::Duration,
+};
 
+use clap::Parser;
 use ledmatrix::LedMatrix;
 
 use crate::widget::{AllCPUsWidget, BatteryWidget, UpdatableWidget};
 
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    // ======== Info about system ========
+    #[arg(long)]
+    /// List all connected matrix modules
+    list_modules: bool,
+
+    /// List all widgets available for placement
+    #[arg(long)]
+    list_widgets: bool,
+
+    // ======== Program Control ========
+    #[arg(long)]
+    /// Start the background service updating the matrix
+    start: bool,
+
+    #[arg(long)]
+    /// JSON config file path
+    config: Option<String>,
+}
+
+enum Program {
+    ListMod,
+    ListWid,
+    Default,
+}
+
 fn main() {
-    // TODO possible options: 
+    // TODO possible options:
     // each widget + Y placement + LED module (both as default) (for now, x maybe later)
     // Overall brightness
     // update rate
 
-    let mats_info = LedMatrix::detect();
+    let mut program = Program::Default;
 
-    if mats_info.len() <= 0 {
-        println!("No LED matrix modules found.");
-        exit(1)
+    if args_os().len() > 1 {
+        let cli = Cli::parse();
+        if cli.list_modules {
+            program = Program::ListMod;
+        } else if cli.list_widgets {
+            program = Program::ListWid;
+        }
     }
 
-    let mut mats: Vec<LedMatrix> = Vec::new();
-    for m in mats_info {
-        mats.push(LedMatrix::new(m));
+    match program {
+        Program::Default => {
+            let mut mats = LedMatrix::detect();
+
+            // No arguments provided? Start the
+            if args().len() <= 1 {
+                let mut b = BatteryWidget::new();
+                let mut c = AllCPUsWidget::new();
+
+                let blank = [[0; 9]; 34];
+                mats[1].draw_matrix(blank);
+
+                loop {
+                    b.update();
+                    c.update();
+
+                    let mut matrix = [[0; 9]; 34];
+                    matrix = matrix::emplace(matrix, Box::from(&mut b), 0, 0);
+                    matrix = matrix::emplace(matrix, Box::from(&mut c), 0, 5);
+                    mats[0].draw_matrix(matrix);
+                    thread::sleep(Duration::from_millis(2000));
+                }
+            }
+        },
+        Program::ListMod => {
+            LedMatrix::detect();
+        },
+        Program::ListWid => println!("Not yet implemented :')"),
+
+        // _ => {}
     }
-    
-    println!("Found LED matrix modules:");
-    for i in mats.iter_mut() {
-        println!("{} - {}", i.port_info.port_name.to_string(), i.get_fw_version());
-    }
 
-    let mut b = BatteryWidget::new();
-    let mut c = AllCPUsWidget::new();
-
-    let blank = [[0;9];34];
-    mats[1].draw_matrix(blank);
-
-    loop {
-        b.update();
-        c.update();
-
-        let mut matrix = [[0;9];34];
-        matrix = matrix::emplace(matrix, Box::from(&mut b), 0, 0);
-        matrix = matrix::emplace(matrix, Box::from(&mut c), 0, 5);
-        mats[0].draw_matrix(matrix);
-        thread::sleep(Duration::from_millis(2000));
-    }
-
+    exit(0);
 }
-
- 
